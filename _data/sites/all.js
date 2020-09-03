99bde8ec7971e91bb937beed414c797c735d0564
STOQE <stoqe@stoqe.com>
2020-09-03 14:14:47 +1000

    ...

---
+++ _data/sites/all.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Shopify", // optional, falls back to object key
+	description: "Now testing",
+	options: {
+		frequency: 60 * 11, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://stali.com.au/",
+		"https://sensorylab.com.au/",
+		"https://ivyandwood.com.au/",
+		"https://www.pushas.com/",
+		"https://www.culturekings.com.au/",
+	]
+};
