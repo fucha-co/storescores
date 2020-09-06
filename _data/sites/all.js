10853528f21a31ecc230d637b67d1f2dbe45cfd7
STOQE <stoqe@stoqe.com>
2020-09-07 08:12:56 +1000

    +

--- _data/sites/all.js
+++ _data/sites/all.js
@@ -5,6 +5,8 @@ module.exports = {
 		frequency: 60 * 1, // (in minutes), 23 hours
 	},
 	urls: [
+		"https://debut-theme-default.myshopify.com/",
+		"https://debut-theme-lightroom.myshopify.com/",
 		"https://teamapparel.com.au/",
 		"https://au.koala.com/",
 		"https://sensorylab.com.au/",
